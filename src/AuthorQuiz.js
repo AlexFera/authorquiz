ffd9a1bf7e9c61999e90a3c144ed410a28a09d0d
alex <alex@vostro.dev.arnia>
2018-11-28 14:04:34 +0200

    added a form

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './AuthorQuiz.css';
 import './bootstrap.min.css';
@@ -14,14 +14,14 @@ function Hero() {
         </div>);
 }
 
-function Book({title, onClick}) {
+function Book({ title, onClick }) {
     return (
-        <div className="answer" onClick={() => {onClick(title);}}>
+        <div className="answer" onClick={() => { onClick(title); }}>
             <h4>{title}</h4>
         </div>);
 }
 
-function Turn({author, books, highlight, onAnswerSelected}) {
+function Turn({ author, books, highlight, onAnswerSelected }) {
     function hightlightToBackgroundColor(highlight) {
         const mapping = {
             'none': '',
@@ -33,14 +33,14 @@ function Turn({author, books, highlight, onAnswerSelected}) {
     }
 
     return (
-    <div className="row turn" style={{backgroundColor: hightlightToBackgroundColor(highlight)}}>
-        <div className="col-4 offset-1">
-            <img src={author.imageUrl} className="authorimage" alt="Author" />
+        <div className="row turn" style={{ backgroundColor: hightlightToBackgroundColor(highlight) }}>
+            <div className="col-4 offset-1">
+                <img src={author.imageUrl} className="authorimage" alt="Author" />
+            </div>
+            <div className="col-6">
+                {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+            </div>
         </div>
-        <div className="col-6">
-            {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />) }
-        </div>
-    </div>
     );
 }
 
@@ -56,8 +56,18 @@ Turn.propTypes = {
     highlight: PropTypes.string.isRequired
 };
 
-function Continue() {
-    return (<div />);
+function Continue({ show, onContinue }) {
+    return (
+        <div className="row continue">
+            {
+                show
+                    ? <div className="col-11">
+                        <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+                    </div>
+                    : null
+            }
+        </div>
+    );
 }
 
 function Footer() {
@@ -70,12 +80,12 @@ function Footer() {
     );
 }
 
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+function AuthorQuiz({ turnData, highlight, onAnswerSelected, onContinue }) {
     return (
         <div className="container-fluid">
             <Hero />
-            <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-            <Continue />
+            <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+            <Continue show={highlight === 'correct'} onContinue={onContinue} />
             <p><Link to="/add">Add an author</Link></p>
             <Footer />
         </div>
